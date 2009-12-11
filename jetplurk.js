e516eff76d6947bf63039aa77628ab739e157cf0
Irvin <irvinfly@gmail.com>
2009-12-12 02:37:57 +0800

    0.0007: show display_name, time, adjust meta style

--- jetplurk.js
+++ jetplurk.js
@@ -1,8 +1,8 @@
-/* JetPlruk 0.0005
- *
- * Some code originaled from BobChao's JetWave 
- *   http://go.bobchao.net/jetwave
- * With the help from littlebtc
+/* JetPlurk 0.0007
+ * cc:by-sa 
+ * Author: Irvin 
+ * With the help from littlebtc & MozTW community
+ * Some code adapted from BobChao's JetWave http://go.bobchao.net/jetwave
  */
 
 var manifest = {
@@ -32,7 +32,7 @@ jetpack.future.import('slideBar')
 jetpack.slideBar.append( {
     icon: "http://www.plurk.com/favicon.ico",
     width: 250,
-    html: "<style>body {margin: 0; padding-top: 55px; background: url(http://www.plurk.com/static/logo.png) top left no-repeat; background-color: white; border-bottom:solid lightgray 1px; font-size: 12px;} msgs {display: block; max-width: 245px; overflow: hidden; } msg {display: block; border-bottom:solid lightgray 1px; position: relative; padding: 2px; min-height: 2.5em;} msg:hover {background-color: lightgreen;}</style><body><div id='container'><msgs></msgs></div></body>",
+    html: "<style>body {margin: 0; padding-top: 55px; background: url(http://www.plurk.com/static/logo.png) top left no-repeat; background-color: white; border-bottom:solid lightgray 1px; font-size: 12px;} msgs {display: block; max-width: 245px; overflow: hidden; } msg {display: block; border-bottom:solid lightgray 1px; position: relative; padding: 2px; min-height: 2.5em;} msg:hover {background-color: lightgreen;} msgs .meta { margin-top:2px; display:block; color: DarkGray; text-align: right; font-size: 0.9em;}</style><body><div id='container'><msgs></msgs></div></body>",
     onClick: function(slider){
 
 		$.ajax({
@@ -46,9 +46,9 @@ jetpack.slideBar.append( {
 				$(jsObject.plurks).each(
 					function(i){
 						var owner_id = jsObject.plurks[i].owner_id;
-						var owner_nickname = jsObject.plurks_users[owner_id].nick_name;
+						var owner_display_name = jsObject.plurks_users[owner_id].display_name;
 						var premalink = jsObject.plurks[i].plurk_id.toString(36)
-						var content = '<msg id=\"' + jsObject.plurks[i].plurk_id + '\">' + owner_nickname + ' [' + jsObject.plurks[i].qualifier + '] ' + jsObject.plurks[i].content + '<br><span class=\"meta\"><a class=\"permalink\" href=\"http://plurk.com/m/p/' + premalink + '\" target=\"_blank\">link</a></span></msg>';
+						var content = '<msg id=\"' + jsObject.plurks[i].plurk_id + '\">' + owner_display_name + ' [' + jsObject.plurks[i].qualifier_translated + '] ' + jsObject.plurks[i].content + '<br><span class=\"meta\">' + jsObject.plurks[i].posted + ' <a class=\"permalink\" href=\"http://www.plurk.com/m/p/' + premalink + '\" target=\"_blank\">link</a></span></msg>';
 						//console.log(content);				
 						$(slider.contentDocument).find("msgs").append(content);
 					}
