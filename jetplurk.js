6617ce65bc9e775a906d1a1c33d94878f6315e86
Irvin <irvinfly@gmail.com>
2009-12-14 03:20:23 +0800

    refresh ok but hover to set read mal function.

--- jetplurk.js
+++ jetplurk.js
@@ -23,11 +23,9 @@ jetpack.future.import("storage.settings");
 set = jetpack.storage.settings;
 
 var plurk_ApiKey = "LGMTGe6MKqjPnplwd4xHkUFTXjKOy6lJ";
-var loginStr = {};  
-loginStr.username = set.jetplurk.username;  
-loginStr.password = set.jetplurk.password;  
-loginStr.api_key = plurk_ApiKey;  
+ 
 
+var sliderObj = null;
 var NewOffset = new Date( );	// To remember latest loaded plurk timestamp
 var OldOffset = new Date( );	// Oldest loaded plurk timestamp
 console.log('Begin: NewOffset ' + NewOffset + ' OldOffset ' + OldOffset);
@@ -40,7 +38,12 @@ jetpack.slideBar.append( {
     html: "<style>body {margin: 0; background-color: white; border-bottom:solid lightgray 1px; font-size: 12px;} #banner {display:block;} #banner img {border:0px; } msgs {display: block; max-width: 245px; overflow: hidden; } msg {display: block; border-bottom:solid lightgray 1px; position: relative; padding: 4px; min-height: 2.5em;} msg:hover {background-color: lightgreen;}  msgs .unread {font-weight: bold;} msgs .meta { margin-top:2px; display:block; color: DarkGray; text-align: right; font-size: 0.9em;}</style><body><div id='banner'><a href='http://www.plurk.com' target='_blank'><img src='http://www.plurk.com/static/logo.png'></a></div><div id='container'><msgs></msgs></div></body>",
 
     onReady: function(slider){	
-    	// When sidebar ready, preform login and get newest plurk
+    	// When sidebar ready, preform reFreshPlurk()
+
+    	sliderObj = slider;
+    	reFreshPlurk();
+
+		/*		
 		$.ajax({
 			url: "http://www.plurk.com/API/Users/login",
 			data: loginStr,
@@ -65,7 +68,7 @@ jetpack.slideBar.append( {
 							content += ' class=\"unread\">';
 						}
 						content += jsObject.plurks[i].content + '</content><br><span class=\"meta\">' + jsObject.plurks[i].posted + ' <a class=\"permalink\" href=\"http://www.plurk.com/m/p/' + premalink + '\" target=\"_blank\">link</a></span></msg>';
-						console.log(content);				
+						//console.log(content);				
 						$(slider.contentDocument).find("msgs").append(content);
 						OldOffset = jsObject.plurks[i].posted;
 					}
@@ -89,11 +92,14 @@ jetpack.slideBar.append( {
 			} 
 
 		});
+		*/
     },
 
 
     
     onClick: function(slider){
+		reFreshPlurk();
+		/*
 	    	var getPlurk = {};
 	    	getPlurk.api_key = plurk_ApiKey;  
 	    	getPlurk.offset = '2009-6-20T21:55:34';  
@@ -148,10 +154,12 @@ jetpack.slideBar.append( {
 			},
 			error:function (xhr, textStatus, errorThrown){
 				// Login error
-				console.log(xhr.status + textStatus + errorThrown);				
+				console.log(xhr.status +" "+textStatus + " " + errorThrown);				
 			} 
 
 		});
+		*/
+		
 	},
     
     onSelect: function(slider){
@@ -198,3 +206,68 @@ jetpack.slideBar.append( {
     },
 
 });
+
+
+function reFreshPlurk() {
+	// When reFreshPlurk, preform login and get newest plurk
+	console.log("reFreshPlurk")
+
+	var loginStr = {};  
+	loginStr.username = set.jetplurk.username;  
+	loginStr.password = set.jetplurk.password;  
+	loginStr.api_key = plurk_ApiKey; 
+
+	$.ajax({
+	url: "http://www.plurk.com/API/Users/login",
+	data: loginStr,
+
+	success: function(json){
+		// When login success, throw the newest plurk come with login
+		var jsObject = JSON.parse(json);
+		//console.log(json)
+		$(sliderObj.contentDocument).find("msg").fadeOut('slow');
+		//$(sliderObj.contentDocument).find("msg").fadeOut('slow').remove();
+
+
+		$(jsObject.plurks).each(
+			function(i){
+				var owner_id = jsObject.plurks[i].owner_id;
+				var owner_display_name = jsObject.plurks_users[owner_id].display_name;
+				var premalink = jsObject.plurks[i].plurk_id.toString(36);
+				var read = jsObject.plurks[i].is_unread;
+				var response_count = jsObject.plurks[i].response_count
+				var content = '<msg id=\"' + jsObject.plurks[i].plurk_id + '\"><span class=\"responseNum\">' + response_count + ' </span>' + owner_display_name + ' [' + jsObject.plurks[i].qualifier_translated + '] <content';
+				if (read == 0){ //read
+					content += '>';
+				}
+				if (read == 1 || jsObject.plurks[i].responses_seen < response_count){	//unread
+					content += ' class=\"unread\">';
+				}
+				content += jsObject.plurks[i].content + '</content><br><span class=\"meta\">' + jsObject.plurks[i].posted + ' <a class=\"permalink\" href=\"http://www.plurk.com/m/p/' + premalink + '\" target=\"_blank\">link</a></span></msg>';
+				//console.log(content);				
+				$(sliderObj.contentDocument).find("msgs").append(content);
+				OldOffset = jsObject.plurks[i].posted;
+			}
+		);
+		
+		NewOffset = (new Date( )).toUTCString();
+		console.log('End login: NewOffset ' + NewOffset + ' OldOffset ' + OldOffset);
+		
+		$(sliderObj.contentDocument).find("msgs").find('a').click(function(e){
+			// Force all link open in new tabs, From littlebtc. 
+			if (this.href) { jetpack.tabs.open(this.href); }
+				e.preventDefault();
+				e.stopPropagation();
+			}
+		);
+		
+								
+	},
+	error:function (xhr, textStatus, errorThrown){
+		// Login error
+		console.log(xhr.status + textStatus + errorThrown);				
+	} 
+
+});
+
+}
