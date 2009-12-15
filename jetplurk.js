80d24992bc4143157cdf56fc0e00152f7e0e7bc7
Irvin <irvinfly@gmail.com>
2009-12-15 20:13:35 +0800

    Add fetch and see responses when click, adjust display style

--- jetplurk.js
+++ jetplurk.js
@@ -47,7 +47,7 @@ jetpack.future.import('slideBar')
 jetpack.slideBar.append( {
     icon: "http://www.plurk.com/favicon.ico",
     width: 250,
-    html: "<style>body {margin: 0; background-color: white; border-bottom:solid lightgray 1px; font-size: 12px;} #banner {display:block;} #banner img {border:0px; } msgs {display: block; max-width: 245px; overflow: hidden; } msg {display: block; border-bottom:solid lightgray 1px; position: relative; padding: 4px; min-height: 2.5em;} msg:hover {background-color: lightgreen;}  msgs .unread {font-weight: bold;} msgs .unreadresponse {color: DarkGreen; font-weight: bold;} msgs .meta { margin-top:2px; display:block; color: DarkGray; text-align: right; font-size: 0.9em;}</style><body><div id='banner'><a href='http://www.plurk.com' target='_blank'><img src='http://www.plurk.com/static/logo.png'></a></div><div id='container'><msgs><msg></msg></msgs></div></body>",
+    html: "<style>body {margin: 0; background-color: BurlyWood; border-bottom:solid lightgray 1px; font-size: 12px; line-height: 1.3em;} #banner {display:block;} #banner img {border:0px; } msgs {display: block; max-width: 245px; overflow: hidden; } msg {display: block; background-color: Snow; -moz-border-radius: 5px; padding: 2px; margin: 2px; min-height: 2.5em;} msg:hover {background-color: White;}  msgs .unread {font-weight: bold;} msgs .unreadresponse {color: DarkGreen; font-weight: bold;} msgs .meta { margin-top:2px; display:block; color: DarkGray; text-align: right; font-size: 0.9em;} responses {display: block; line-height: 1.1em; overflow: hidden; margin:2px; border: solid lightgray thin; -moz-border-radius: 5px; padding: 2px;} response {display: block; margin:0;} </style><body><div id='banner'><a href='http://www.plurk.com' target='_blank'><img src='http://www.plurk.com/static/logo.png'></a></div><div id='container'><msgs><msg></msg></msgs></div></body>",
 
 	onReady: function(slider){	
 		// When sidebar ready, preform reFreshPlurk()
@@ -89,7 +89,7 @@ function reFreshPlurk() {
 					var response_count = jsObject.plurks[i].response_count;
 					var response_seen = jsObject.plurks[i].responses_seen;
 					var postedtime = jsObject.plurks[i].posted;
-					var content = '<msg id=\"' + jsObject.plurks[i].plurk_id + '\"><span class=\"responseNum\">' + response_seen + ' ' + response_count + ' ' + read + ' </span>' + owner_display_name + ' [' + jsObject.plurks[i].qualifier_translated + '] <content';
+					var content = '<msg id=\"' + jsObject.plurks[i].plurk_id + '\"><span class=\"responseNum\">' + response_count  + '</span> ' + response_seen + ' ' + read + ' ' + owner_display_name + ' [' + jsObject.plurks[i].qualifier_translated + '] <content';
 					if ((read == 1) || ((ReadOffset < Date.parse(postedtime)) && (response_count == 0))){	// If message is unread
 						content += ' class=\"unread\">';
 					}else if (response_seen < response_count) {	// If message response num. higher than seen-responses number
@@ -104,6 +104,8 @@ function reFreshPlurk() {
 				}
 			);
 			
+			NewOffset = Date.parse(new Date());	// Rememver refresh time
+			console.log('End refresh: NewOffset ' + NewOffset + ' OldOffset ' + OldOffset + ' ReadOffset ' + ReadOffset);
 			
 			// Add hover event listener on each msg
 			$(sliderObj.contentDocument).find("msg").hover(
@@ -140,13 +142,62 @@ function reFreshPlurk() {
 					}
 				},
 				function () {
+					var hoverMsg = $(this);
 					//console.log("unHOVER!");
+					if ($(hoverMsg).find("responses").text() != ""){
+						// If showing response now, remove it
+						$(hoverMsg).find("responses").fadeOut('fast',function (){
+							$(hoverMsg).find("responses").remove();
+						});
+					}
 				}
 			);
-			
-			NewOffset = Date.parse(new Date());	// Rememver refresh time
-			console.log('End refresh: NewOffset ' + NewOffset + ' OldOffset ' + OldOffset + ' ReadOffset ' + ReadOffset);
-			
+
+			// Add click event listener on each msg
+			$(sliderObj.contentDocument).find("msg").click(
+				function () {
+					var clickMsg = $(this);
+					var selectPlurkID = parseInt(clickMsg.attr("id"));
+					var selectPlurkResponseNum = clickMsg.find("span.responseNum").text();
+					console.log('Click: ' + selectPlurkID + ' responseNum ' + selectPlurkResponseNum);
+
+					if ((selectPlurkResponseNum != '0') && ($(clickMsg).find("responses").text() == "")){
+						// If click msg has response & not showing now, get response
+						$.ajax({
+							url: "http://www.plurk.com/API/Responses/get",
+							data: ({
+								'api_key': loginStr.api_key,
+								'plurk_id': selectPlurkID,
+								'from_response': 0,
+							}),
+							success: function(json){
+								//console.log('Get response: ' + json);
+								var jsObject = JSON.parse(json);
+
+								// Display each response
+								$(clickMsg).append('<responses></responses>');
+								$(jsObject.responses).each(
+									function(i){
+										var responser_id = jsObject.responses[i].user_id;
+										var responser_display_name = jsObject.friends[responser_id].display_name;
+										var postedtime = jsObject.responses[i].posted;
+										var content = '<response>' + responser_display_name + ' [' + jsObject.responses[i].qualifier_translated + '] ' + jsObject.responses[i].content + ' <span class=\"meta\"><timestamp>' + postedtime + '</timestamp></span></response>';
+										//console.log(content);
+										$(clickMsg).find("responses").append(content);
+									}
+								);
+								//console.log($(clickMsg).html());
+							},
+							error: function(xhr, textStatus, errorThrown){
+								console.log('Get response error: ' + xhr.status + ' ' + textStatus + ' ' + errorThrown);				
+							} 
+						});
+					}
+
+				}
+			);
+
+						
 			$(sliderObj.contentDocument).find("msgs").find('a').click(function(e){
 				// Force all link open in new tabs, From littlebtc. 
 				if (this.href) { jetpack.tabs.open(this.href); }
